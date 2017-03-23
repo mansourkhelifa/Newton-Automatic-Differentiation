cf651b0eb598f75791b2b69f40f3490ffcddfec2
Mansour Khelifa <mk812@ic.ac.uk>
2017-03-23 14:14:39 +0100

    adding files

---
+++ FAD.h
@@ -0,0 +1,122 @@
+#ifndef FAD_H
+#define FAD_H
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class FAD
+{
+public:
+
+fff
+
+//Constructor definition
+
+    FAD()
+    {
+        val_=0;
+        der_=0;
+    }
+
+
+    double getVal()
+    {
+        return val_;
+    }
+
+    double getDer()
+    {
+        return der_;
+    }
+
+
+    FAD(double val, double der):val_(val), der_(der)
+    {
+
+    }
+
+    void operator*=(const FAD & A)
+    {
+
+
+        double a=A.der_*val_+der_*A.val_;
+        der_=a;
+
+        val_*=A.val_;
+
+   }
+
+    void operator+=(const FAD & A)
+    {
+        double a;
+        a=A.der_+der_;
+        der_=a;
+        val_+=A.val_;
+    }
+
+
+    void operator=(const FAD & A)
+    {
+       val_=A.val_;
+       der_=A.der_;
+    }
+
+
+    template <class Ostream>
+    Ostream& print(Ostream& s) const
+    {
+       s<<"value: "<<val_<<"; derivative: "<<der_<<endl;
+
+        return s;
+    }
+
+
+
+private:
+//Value derivative
+double val_;
+//Value derivative
+double der_;
+
+};
+
+ template <typename T>
+ FAD<T> operator*( FAD<T> & A, FAD<T> &  B)
+ {
+     FAD<T> C;
+
+     C=A;
+
+
+     C*=B;
+
+
+     return C;
+
+ }
+
+ template <typename T>
+ FAD<T> operator+( FAD<T> & A, FAD<T> &  B)
+ {
+     FAD<T> C;
+
+     C=A;
+
+
+     C+=B;
+
+
+     return C;
+ }
+
+
+ template <class Ostream, typename T>
+ ostream & operator <<(Ostream& s, const FAD<T> & A)
+ {
+   return A.print(s);
+ }
+
+
+
+#endif // FAD_H
